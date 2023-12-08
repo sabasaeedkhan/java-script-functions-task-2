13441fba27de0200d59e38758fe0334ecdec30c2
Saba Saeed khan <144582707+saeedsamina@users.noreply.github.com>
2023-12-09 01:13:38 +0500

    pick up greatest value

---
+++ script.mjs
@@ -0,0 +1,11 @@
+function konBaraHa(a, b) {
+    if (a > b) {
+        console.log("A bara ha")
+    }
+    else if (b > a) {
+        console.log("B bara ha")
+    }
+    else {
+        console.log("koi bara nhi ha")
+    }
+}
